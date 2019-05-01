2dab59883146581c7bddb85fdc053b5f1d9faed3
idemockle <iank1226@yahoo.com>
2019-05-01 16:15:25 -0400

    More efficient matmul; always traverse matrices row-wise

--- c/matrixmath.c
+++ c/matrixmath.c
@@ -10,8 +10,8 @@ Matrix2D* matmul(Matrix2D* mat1, Matrix2D* mat2) {
     Matrix2D* out = matrix2d_init(mat1->shape[0], mat2->shape[1]);
     
     for (size_t i = 0; i < mat1->shape[0]; i++) {
-        for (size_t j = 0; j < mat2->shape[1]; j++) {
-            for (size_t k = 0; k < mat1->shape[1]; k++) {
+        for (size_t k = 0; k < mat1->shape[1]; k++) {
+            for (size_t j = 0; j < mat2->shape[1]; j++) {
                 out->i[i][j] += mat1->i[i][k] * mat2->i[k][j];
     }}}
 
